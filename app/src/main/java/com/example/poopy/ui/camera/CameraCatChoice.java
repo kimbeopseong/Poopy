6114e2c58347eb413644a714a4679816ac385059
LichRich <leech135789@gmail.com>
2020-08-07 22:14:12 +0900

    Making Camera...(2)
    1. CameraPreview 구현
     - cat_choice_item 레이아웃 구현 -> adapter 구현에 사용
     - DB에 저장 기능 구현.---(O)
     - OpenCV 관련 기능 구현X.
    2. ListFragment -> ListCatChoice로 변경
     - Navigation도 ListCatChoice로 연결.
    3. ResultActivity 구현
     - result 레이아웃과 연결

--- app/src/main/java/com/example/poopy/ui/camera/CameraCatChoice.java
+++ app/src/main/java/com/example/poopy/ui/camera/CameraCatChoice.java
@@ -1,7 +1,9 @@
 package com.example.poopy.ui.camera;
 
+import android.content.Intent;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.paging.PagedList;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.poopy.utils.Cat;
@@ -20,7 +23,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class CameraCatChoice extends Fragment {
+public class CameraCatChoice extends Fragment implements CameraCatChoiceAdapter.OnListItemClick {
         private static final String TAG = "CAMERA CAT FRAGMENT";
 
         private View camera_catChoice;
@@ -47,18 +50,33 @@ public class CameraCatChoice extends Fragment {
 
                 FirestorePagingOptions<Cat> options = new FirestorePagingOptions.Builder<Cat>()
                         .setLifecycleOwner(this)
-                        .setQuery(db.collection("Pet").whereEqualTo("p_ID", currintUID), config, new SnapshotParser<Cat>() {
+                        .setQuery(db.collection("User").document(currintUID).collection("Cat").whereEqualTo("Name", true), config, new SnapshotParser<Cat>() {
                                 @NonNull
                                 @Override
                                 public Cat parseSnapshot(@NonNull DocumentSnapshot snapshot) {
                                         Cat cat = snapshot.toObject(Cat.class);
-                                        cat.setCatName(snapshot.get("p_name").toString());
-                                        cat.setProfile(snapshot.get("p_uri").toString());
+                                        cat.setCatName(snapshot.get("c_name").toString());
+                                        cat.setProfile(snapshot.get("c_uri").toString());
                                         return cat;
                                 }
                         }).build();
 
-                return super.onCreateView(inflater, container, savedInstanceState);
+                CameraCatChoiceAdapter choiceAdapter = new CameraCatChoiceAdapter(options, this);
+
+                catChoiceList = (RecyclerView) camera_catChoice.findViewById(R.id.cat_choice_view);
+                catChoiceList.setLayoutManager(new GridLayoutManager(getContext(), 2));
+                catChoiceList.addItemDecoration(new ItemDecoration(2, 50));
+                catChoiceList.setAdapter(choiceAdapter);
+
+                return camera_catChoice;
+        }
+
+        @Override
+        public void onItemClick(DocumentSnapshot snapshot, int position) {
+                Log.d("ITEM_CLICK", "Clicked an item: " + position + ", id: " + snapshot.getString("Name"));
+                Intent intent = new Intent(this.getContext(), CameraActivity.class);
+                intent.putExtra("Name", snapshot.getString("Name"));
+                startActivity(intent);
         }
 
         public class ItemDecoration extends RecyclerView.ItemDecoration{
